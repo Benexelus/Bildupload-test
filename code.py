6f2ad1c45a91f8f4ea15a9c8853f9e26927d5f6d
Benexelus <It_bene@gmx.de>
2026-02-17 13:01:24 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,38 @@
 import streamlit as st
-   from keras.models import load_model
-   from PIL import Image, ImageOps
-   import numpy as np
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
 
-   # Disable scientific notation for clarity
-   np.set_printoptions(suppress=True)
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-   # Load the model and labels
-   model = load_model("keras_Model.h5", compile=False)
-   class_names = open("labels.txt", "r").readlines()
+# Load the model and labels
+model = load_model("keras_Model.h5", compile=False)
+class_names = open("labels.txt", "r").readlines()
 
-   st.title("Pferd, Esel oder Zebra?")
-   st.write("Lade ein Bild hoch und lass es klassifizieren!")
+st.title("Pferd, Esel oder Zebra?")
+st.write("Lade ein Bild hoch und lass es klassifizieren!")
 
-   uploaded_file = st.file_uploader("Bild hochladen...", type=["jpg", "jpeg", "png"])
-   if uploaded_file is not None:
-       # Display the uploaded image
-       image = Image.open(uploaded_file).convert("RGB")
-       st.image(image, caption='Hochgeladenes Bild', use_column_width=True)
+uploaded_file = st.file_uploader("Bild hochladen...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption='Hochgeladenes Bild', use_column_width=True)
 
-       # Preprocess the image
-       size = (224, 224)
-       image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-       image_array = np.asarray(image)
-       normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-       data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-       data[0] = normalized_image_array
+    # Preprocess the image
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
 
-       # Predict the model
-       prediction = model.predict(data)
-       index = np.argmax(prediction)
-       class_name = class_names[index]
-       confidence_score = prediction[0][index]
+    # Predict the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
 
-       # Display the prediction
-       st.write(f"**Klasse:** {class_name[2:]}")
-       st.write(f"**Confidence Score:** {confidence_score:.2f}")
+    # Display the prediction
+    st.write(f"**Klasse:** {class_name[2:]}")
+    st.write(f"**Confidence Score:** {confidence_score:.2f}")
